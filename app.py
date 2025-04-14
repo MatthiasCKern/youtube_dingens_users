d2de11c008a009cd6a77463a74c2700fc0db864d
Ujzxz <ujzxz@student.kit.edu>
2025-04-14 23:42:06 +0200

    hash laenger

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class User(db.Model):
     __tablename__ = "users"  # Explicitly set to avoid SQL keyword conflict
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     events = db.relationship('WatchEvent', backref='user', lazy=True, cascade="all, delete-orphan")
 
 class WatchEvent(db.Model):
